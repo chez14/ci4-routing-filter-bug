2ae16a46481eae0a881bb40f659eb6c755053e9e
Chris "chez14" Qiang <chris@christianto.net>
2021-04-14 15:35:12 +0700

    Add routing, filters and login view for testing

--- extendables/app/Controllers/RestrictedArea.php
+++ extendables/app/Controllers/RestrictedArea.php
@@ -4,6 +4,11 @@ namespace Extendables\App\Controllers;
 
 class RestrictedArea extends BaseController
 {
+    public function get_login()
+    {
+        return view('Extendables\App\Views\user_login');
+    }
+
     public function index()
     {
         return view('Extendables\App\Views\admin_page');
