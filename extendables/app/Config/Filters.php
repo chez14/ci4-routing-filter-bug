f0c64f89d67b37b6b56f0373d55f8ec9ec690ecd
Chris "chez14" Qiang <chris@christianto.net>
2021-04-24 11:26:26 +0700

    Implement @MGatner's suggesttion

--- extendables/app/Config/Filters.php
+++ extendables/app/Config/Filters.php
@@ -2,63 +2,12 @@
 
 namespace Extendables\Config;
 
-use CodeIgniter\Config\BaseConfig;
-use CodeIgniter\Filters\CSRF;
-use CodeIgniter\Filters\DebugToolbar;
-use CodeIgniter\Filters\Honeypot;
-use Extendables\App\Controllers\Auth;
+use Config\Filters;
+use Extendables\Controllers\Auth;
 
-class Filters extends BaseConfig
-{
-	/**
-	 * Configures aliases for Filter classes to
-	 * make reading things nicer and simpler.
-	 *
-	 * @var array
-	 */
-	public $aliases = [
-		'csrf'     => CSRF::class,
-		'toolbar'  => DebugToolbar::class,
-		'honeypot' => Honeypot::class,
-		'pnd_auth' => Auth::class,
-	];
-
-	/**
-	 * List of filter aliases that are always
-	 * applied before and after every request.
-	 *
-	 * @var array
-	 */
-	public $globals = [
-		'before' => [
-			// 'honeypot',
-			// 'csrf',
-		],
-		'after'  => [
-			'toolbar',
-			// 'honeypot',
-		],
-	];
-
-	/**
-	 * List of filter aliases that works on a
-	 * particular HTTP method (GET, POST, etc.).
-	 *
-	 * Example:
-	 * 'post' => ['csrf', 'throttle']
-	 *
-	 * @var array
-	 */
-	public $methods = [];
-
-	/**
-	 * List of filter aliases that should run on any
-	 * before or after URI patterns.
-	 *
-	 * Example:
-	 * 'isLoggedIn' => ['before' => ['account/*', 'profiles/*']]
-	 *
-	 * @var array
-	 */
-	public $filters = [];
-}
+/**
+ * @var Filters $filters
+ * 
+ * @link https://github.com/codeigniter4/CodeIgniter4/issues/4572#issuecomment-821981736
+ */
+$filters->aliases['pnd_auth'] = Auth::class;
